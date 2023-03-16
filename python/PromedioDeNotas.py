392b5cde110e6774d3c6c55dae10c7babbb96e2f
AnonSYS23 <127991043+AnonSYS23@users.noreply.github.com>
2023-03-16 16:43:06 -0500

    Programacion_curso

--- python/PromedioDeNotas.py
+++ python/PromedioDeNotas.py
@@ -1,14 +1,10 @@
 # promedio de 5 notas
 # sumas las 5 notas y todo lo divides por 5
-nota1 = 4
-nota2 = 5
-nota3 = 4
-nota4 = 2
-nota5 = 1
-resultado = 3.2
+nota1 = float(input("Su primera nota es :"))
+nota2 = float(input("Su segunda nota es :"))
+nota3 = float(input("Su tercera nota es :"))
+nota4 = float(input("Su cuarta nota es :"))
+nota5 = float(input("Su quinta nota es :"))
 
-resultado = ((nota1 + nota2 + nota3 + nota4 + nota5)/ 5)
-
-print(resultado)
-
-print(resultado + 2.5)
+operacion = nota1 + nota2 + nota3 + nota4 + nota5 /5
+print(f'su resultado es: {operacion}')
