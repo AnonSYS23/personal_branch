a5c9ac17c9e9a46aeafb352309bf3f30e2ba8e34
AnonSYS23 <127991043+AnonSYS23@users.noreply.github.com>
2023-03-17 20:09:17 -0500

    Programacion_curso

--- python/matchcase3.py
+++ python/matchcase3.py
@@ -1,10 +1,31 @@
-user = str(input('Ingrese su nombre aca porfavor :'))
-number = float(input("Escribe un numero aqui porfavor :"))
-user.capitalize()
+print('Bienvenido a Calendario 1.1')
 
-if number > 0:
-    print(f"{user.capitalize()} su numero es positivo")
-elif number < 0:
-    print(f"{user.capitalize()} su numero es negativo")
-else:
-    print(f"{user.capitalize()} su numero es neutro")
+month = int(input('Inserte el mes que quiere ingresar del 1 al 12 :'))
+
+match month:
+    case 1:
+        print('Enero')
+    case 2:
+        print('Febrero')
+    case 3:
+        print('Marzo')
+    case 4:
+        print('Abril')
+    case 5:
+        print('Mayo')
+    case 6:
+        print('Junio')
+    case 7:
+        print('Julio')
+    case 8:
+        print('Agosto')
+    case 9:
+        print('Septiembre')
+    case 10:
+        print('Octubre')
+    case 11:
+        print('Noviembre')
+    case 12:
+        print('Diciembre')
+    case _:
+        print('Mes invalido, intente mas tarde')
