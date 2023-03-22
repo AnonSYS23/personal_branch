6febfe78cae2ddb90a310f6ef664112a155600fe
AnonSYS23 <127991043+AnonSYS23@users.noreply.github.com>
2023-03-22 17:32:26 -0500

    Programacion_curso

--- python/bucles/buclesintroduccion.py
+++ python/bucles/buclesintroduccion.py
@@ -8,3 +8,23 @@
 # while contador<10:
 #     contador+=1
 #     print(contador)
+
+# counter = 0
+# while counter < 20:
+#   print(counter)
+#   counter+=1
+#   if counter == 15:
+#     break
+# print('Fin del Programa')
+
+# counter = 0
+# while counter < 20:
+#   counter+=1
+#   if counter < 15:
+#     continue
+#   print(counter)
+# print('Fin del Programa')
+
+
+for i in range(1,100,5):
+    print(i)
