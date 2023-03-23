23cc8806718862332742c25228926002defb8307
AnonSYS23 <127991043+AnonSYS23@users.noreply.github.com>
2023-03-23 17:10:41 -0500

    Programacion_curso

---
+++ python/for/for_introduccion.py
@@ -0,0 +1,79 @@
+# for variable in elemento iterable (lista, cadena, range, etc.):
+#     cuerpo del bucle
+
+# print("Comienzo")
+# for i in [0, 1, 2]:
+#     print("Hola ", end="")
+# print()
+# print("Final")
+
+# print("Comienzo")
+# for i in []:
+#     print("Hola ", end="")
+# print()
+# print("Final")
+
+# print("Comienzo")
+# for i in [1, 1, 1]:
+#     print("Hola ", end="")
+# print()
+# print("Final")
+
+# print("Comienzo")
+# for _ in [0, 1, 2]:
+#     print("Hola ", end="")
+# print()
+# print("Final")
+
+# print("Comienzo")
+# for i in [3, 4, 5]:
+#     print(f"Hola. Ahora i vale {i} y su cuadrado {i ** 2}")
+# print("Final")
+
+# print("Comienzo")
+# for i in ["Alba", "Benito", 27]:
+#     print(f"Hola. Ahora i vale {i}")
+# print("Final")
+
+# print("Comienzo")
+# for numero in  [0, 1, 2, 3]:
+#     print(f"{numero} * {numero} = {numero ** 2}")
+# print("Final")
+
+# import random
+
+# print("Comienzo")
+# sacaste_cinco = False
+# for i in range(3):
+#     dado = random.randrange(1, 7)
+#     print(f"Tirada {i + 1}: {dado}")
+#     if dado == 5:
+#         sacaste_cinco = True
+# if sacaste_cinco:
+#     print("Ha salido al menos un 5.")
+# else:
+#     print("No ha salido ningún 5.")
+# print("Final")
+
+# import random
+
+# print("Comienzo")
+# cuenta_cincos = 0
+# for i in range(3):
+#     dado = random.randrange(1, 7)
+#     print(f"Tirada {i + 1}: {dado}")
+#     if dado == 5:
+#         cuenta_cincos += 1
+# print(f"En total ha(n) salido {cuenta_cincos} cinco(s).")
+# print("Final")
+
+# import random
+
+# print("Comienzo")
+# total = 0
+# for i in range(3):
+#     dado = random.randrange(1, 7)
+#     print(f"Tirada {i + 1}: {dado}")
+#     total += dado
+# print(f"En total ha obtenido {total} punto(s).")
+# print("Final")
