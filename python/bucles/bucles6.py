e79585708df94cfe4909c8f0a012ae972bac71eb
AnonSYS23 <127991043+AnonSYS23@users.noreply.github.com>
2023-03-23 16:57:03 -0500

    Programacion_curso

---
+++ python/bucles/bucles6.py
@@ -0,0 +1,16 @@
+passwd = 'programmer'
+counter = 0
+
+print('Bienvenido, ingrese su contraseña para entrar')
+
+passwd1 = str(input('Introduzca la contraseña :'))
+
+while passwd != passwd1:
+    passwd1 = str(input('Introduzca la contraseña denuevo :'))
+    counter += 1
+    if counter == 3:
+        print('Acceso bloqueado a la cuenta')
+        break
+    elif passwd == passwd1:
+        print('Acceso concedido')
+        break
