fb1da35c5be60a029c20937192e344dba78c94ba
AnonSYS23 <127991043+AnonSYS23@users.noreply.github.com>
2023-03-24 16:59:21 -0500

    Programacion_curso

---
+++ python/listas/listas_introduccion.py
@@ -0,0 +1,24 @@
+# lista = [1,2,3,4,5]
+# lista.append(6)
+# lista
+
+
+
+
+# []
+
+# l1 = [1,2,3]
+# l2 = [4,5,6]
+# l1.extend(l2)
+
+# l1
+
+# [1, 2, 3, 4, 5, 6]
+
+
+# lista = [1,2,3]
+# lista2 = [4,5,6]
+
+# for lista, lista2 in zip(lista,lista2):
+#     print(lista)
+#     print(lista2)
