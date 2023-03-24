49e57c7ae9fcb1a4ecb9ce290c873490431fc1a9
AnonSYS23 <127991043+AnonSYS23@users.noreply.github.com>
2023-03-24 16:44:39 -0500

    Programacion_curso

---
+++ python/for/for4.py
@@ -0,0 +1,4 @@
+i = int(input('Ingrese un numero aqui :'))
+
+for i in range(i, 0,-1):
+    print(i, end=',')
---
+++ python/for/for6.py
@@ -0,0 +1,6 @@
+i = 0
+
+for i in range(0,10):
+    i = i + 1
+    x = 10 * i
+    print(x)
