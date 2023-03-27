4fbd1a52a0d15b4f73fadf9e744631f133134a49
AnonSYS23 <127991043+AnonSYS23@users.noreply.github.com>
2023-03-27 16:53:02 -0500

    Programacion_curso

--- python/bucles/bucles1.py
+++ python/bucles/bucles1.py
@@ -2,3 +2,6 @@ contador = 0
 while contador<50:
     contador+=2
     print(contador)
+
+for i in range(0,51,2):
+    print(i)
--- python/bucles/bucles6.py
+++ python/bucles/bucles6.py
@@ -8,7 +8,7 @@ passwd1 = str(input('Introduzca la contraseña :'))
 while passwd != passwd1:
     passwd1 = str(input('Introduzca la contraseña denuevo :'))
     counter += 1
-    if counter == 3:
+    if counter == 2:
         print('Acceso bloqueado a la cuenta')
         break
     elif passwd == passwd1:
