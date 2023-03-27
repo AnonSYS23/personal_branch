518e4584f1555d5780c59d021f5e49afbdad63c2
AnonSYS23 <127991043+AnonSYS23@users.noreply.github.com>
2023-03-27 17:23:29 -0500

    Programacion_curso

---
+++ python/funciones/funciones_introduccion.py
@@ -0,0 +1,13 @@
+def run():
+
+    user = str(input('Ingrese su nombre :'))
+
+    def Ejemplo1 (x, y):
+        return x+y
+
+
+    if Ejemplo1(15, 18) > 18:
+        print(f'{user}, su edad actual que es {Ejemplo1(15, 18)} es mayor de 18 años')
+
+if __name__ == '__main__':
+    run()
