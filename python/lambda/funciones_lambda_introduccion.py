5d2cf50978a5bc501ad6c48c01c35d6e3291358a
AnonSYS23 <127991043+AnonSYS23@users.noreply.github.com>
2023-03-29 17:52:22 -0500

    Programacion_curso

---
+++ python/lambda/funciones_lambda_introduccion.py
@@ -0,0 +1,28 @@
+suma = lambda num1,num2,num3 : num1 + num2 + num3
+
+result = suma(20,30,40)
+
+print(result)
+
+
+resta = lambda num1,num2,num3 : num1 - num2 - num3
+
+result = resta(20,30,40)
+
+print(result)
+
+
+
+multiplicacion = lambda num1,num2,num3 : num1 * num2 * num3
+
+result = multiplicacion(20,30,40)
+
+print(result)
+
+
+
+division = lambda num1,num2,num3 : num1 / num2 / num3
+
+result = division(20,30,40)
+
+print(result)
